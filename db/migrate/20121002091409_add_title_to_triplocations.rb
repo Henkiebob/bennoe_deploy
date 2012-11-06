e6350aafcc54bd5b4f96e438591215f1e583f5cf
Tjerk Dijkstra <tjerk.dijkstra@gmail.com>
2012-11-06 11:29:53 +0100

    extra trups
    asdasd

--- db/migrate/20121002091409_add_title_to_triplocations.rb
+++
@@ -1,5 +0,0 @@
-class AddTitleToTriplocations < ActiveRecord::Migration
-   def change
-	add_column :triplocation, :title, :string
-  end
-end
--- db/migrate/20121016164429_add_column_to_trips.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToTrips < ActiveRecord::Migration
-  def change
-    add_column :trips, :category_id, :integer
-  end
-end
